fbe1e87d93792b521fc43c8dec246de20fb3e459
Akash Kalamkar <akash.kalamkar1995@gmail.com>
2026-05-15 11:44:30 +0530

    added main order

--- src/main/java/com/demo/demogit/Order.java
+++ src/main/java/com/demo/demogit/Order.java
@@ -5,5 +5,6 @@ public class Order {
     void test(){
         System.out.println("order place");
         System.out.println("manual order place");
+        System.out.println("main order place");
     }
 }
